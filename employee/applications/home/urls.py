bc7bfe2f592e1032982d8a56316249dd69ff7e8f
Arnol Plazas <plazasarnol@gmail.com>
2023-02-06 09:56:56 -0500

    I created a model and a ListView to show register of the model finally I added one on admin page.

--- employee/applications/home/urls.py
+++ employee/applications/home/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('home/', views.IndexView.as_view()),
     path('list/', views.TestListView.as_view()),
+    path('model_list/', views.ModelListView.as_view()),
 ]
--- employee/applications/home/views.py
+++ employee/applications/home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
-
+from .models import Test
 
 class IndexView(TemplateView):
     template_name = 'home/home.html'
@@ -11,3 +11,7 @@ class TestListView(ListView):
     queryset = ['A', 'B', 'C']
     context_object_name = 'test_list'
     
+class ModelListView(ListView):
+    model = Test
+    template_name = 'home/model_list.html'
+    context_object_name = 'model_list'

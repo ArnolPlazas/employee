fd7a289e2344d97e99abfb12f5dae8bdbe4cbc60
Arnol Plazas <plazasarnol@gmail.com>
2023-02-14 10:29:31 -0500

    BD in Django manage.py

--- employee/applications/departments/models.py
+++ employee/applications/departments/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Department(models.Model):
+    name = models.CharField('name', max_length=50, blank=True)
+    short_name = models.CharField('short name', max_length=20, unique=True)
+    is_activate = models.BooleanField('activate?', default=False)
+    
+
+    def __str__(self) -> str:
+        return str(self.id) + ' ' +  self.name
--- employee/applications/employees/models.py
+++ employee/applications/employees/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from applications.departments.models import Department
+
+class Employee(models.Model):
+    """Employee model for table on bd"""
+    JOB_CHOICES =(
+        ('0', 'Engineer'),
+        ('1', 'Accounter'),
+        ('2', 'Salesman'),
+        ('3', 'Others'),
+    )
+    first_name = models.CharField('name', max_length=60)
+    last_name = models.CharField('last name', max_length=60)
+    job = models.CharField('job', max_length=1, choices=JOB_CHOICES)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    # image = models.ImageField(, upload_to=None, height_field=None, width_field=None, max_length=None)
+
+    def __str__(self) -> str:
+        return str(self.id) + ' ' +  self.first_name + ' ' + self.last_name

904ff35f3f1e8a559743122567b94a0909676833
Arnol Plazas <plazasarnol@gmail.com>
2023-02-15 10:10:06 -0500

    filter employee by employee using ListView.

--- employee/applications/employees/urls.py
+++ employee/applications/employees/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('list-all-employee/', views.ListAllEmployee.as_view()),
+    path('list-employee-by-area/', views.ListEmployeeByArea.as_view()),
 ]
--- employee/applications/employees/views.py
+++ employee/applications/employees/views.py
@@ -4,9 +4,14 @@ from django.views.generic import ListView
 #models
 from .models import Employee
 
-# list all employee
+
 class ListAllEmployee(ListView):
+    """list all employee"""
     template_name = 'employee/list_all.html'
     model = Employee
-    context_object_name = 'lista'
+    context_object_name = 'lista' # por defecto es object_list
 
+class ListEmployeeByArea(ListView):
+    """list all employee by area"""
+    template_name = 'employee/list_employee_by_area.html'
+    queryset = Employee.objects.filter(department__name='TECNOLOGIA')
